243996949ac79dc249d7bfcd4b112f40ebc0eb7b
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2017-02-05 02:55:28 +0900

    Menu - Plan - Multiple Choice

--- app/src/main/java/earphone/unithon4th/tripear/Map.java
+++ app/src/main/java/earphone/unithon4th/tripear/Map.java
@@ -4,8 +4,8 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.util.SparseBooleanArray;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,7 +19,7 @@ import earphone.unithon4th.tripear.recyclerview.Map_Fragment;
 public class Map extends FragmentActivity {
 
     EditText searchEdit;
-    Button add_listBtn;
+    Button add_listBtn, delete_listBtn;
     ListView listView;
 
     ArrayList<String> list;
@@ -32,7 +32,9 @@ public class Map extends FragmentActivity {
 
         searchEdit = (EditText)findViewById(R.id.searchEdit);
         add_listBtn = (Button)findViewById(R.id.add_listBtn);
+        delete_listBtn = (Button)findViewById(R.id.delete_listBtn);
         listView = (ListView)findViewById(R.id.addedAreaList);
+
 
         Map_Fragment fragment1 = new Map_Fragment();
         fragment1.setArguments(new Bundle());
@@ -42,23 +44,36 @@ public class Map extends FragmentActivity {
         fragmentTransaction.commit();
 
         list = new ArrayList<>();
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_single_choice, list);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, list);
         listView.setAdapter(adapter);
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(Map.this, list.get(position).toString(), Toast.LENGTH_SHORT).show();
-            }
-        });
         add_listBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 int count = list.size();
-                list.add((count + 1) + " " + searchEdit.getText().toString());
-                adapter.notifyDataSetChanged();
-                searchEdit.setText("");
+                if(searchEdit.getText().length() == 0){
+                    Toast.makeText(Map.this, "원하는 곳을 입력하세요", Toast.LENGTH_LONG).show();
+                }else{
+                    list.add(searchEdit.getText().toString());
+                    adapter.notifyDataSetChanged();
+                    searchEdit.setText("");
+                }
             }
+        });
+
+        delete_listBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SparseBooleanArray checkedItems = listView.getCheckedItemPositions();
+                int count = adapter.getCount();
+
+                for(int i = count - 1; i >= 0; i--){
+                    if(checkedItems.get(i)) list.remove(i);
+                }
+                listView.clearChoices();
+                adapter.notifyDataSetChanged();
+            }
+
         });
     }
 }
